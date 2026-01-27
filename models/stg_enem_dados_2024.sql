174962e698ad6bd709eb89ec6455b0e02e1b3f68
Victor Capel <victorsturnichcapel@gmail.com>
2026-01-27 01:09:46 +0000

    update columns

--- models/stg_enem_dados_2024.sql
+++ models/stg_enem_dados_2024.sql
@@ -1,47 +1,78 @@
--- Este comando diz ao dbt para criar uma TABELA no BigQuery
-{{ config(materialized='table') }}
+{{ config(
+    materialized='view',
+    tags=['enem', 'staging']
+) }}
 
-SELECT 
-    NU_SEQUENCIAL AS id,
-    NU_ANO AS year_test,
-    CO_ESCOLA AS id_school,
-    CO_MUNICIPIO_ESC,
-    NO_MUNICIPIO_ESC,
-    CO_UF_ESC,
-    SG_UF_ESC,
-    TP_DEPENDENCIA_ADM_ESC,
-    TP_LOCALIZACAO_ESC,
-    TP_SIT_FUNC_ESC,
-    CO_MUNICIPIO_PROVA,
-    NO_MUNICIPIO_PROVA,
-    CO_UF_PROVA,
-    SG_UF_PROVA,
-    TP_PRESENCA_CN,
-    TP_PRESENCA_CH,
-    TP_PRESENCA_LC,
-    TP_PRESENCA_MT,
-    CO_PROVA_CN,
-    CO_PROVA_CH,
-    CO_PROVA_LC,
-    CO_PROVA_MT,
-    NU_NOTA_CN,
-    NU_NOTA_CH,
-    NU_NOTA_LC,
-    NU_NOTA_MT,
-    TX_RESPOSTAS_CN,
-    TX_RESPOSTAS_CH,
-    TX_RESPOSTAS_LC,
-    TX_RESPOSTAS_MT,
-    TP_LINGUA,
-    TX_GABARITO_CN,
-    TX_GABARITO_CH,
-    TX_GABARITO_LC,
-    TX_GABARITO_MT,
-    TP_STATUS_REDACAO,
-    NU_NOTA_COMP1,
-    NU_NOTA_COMP2,
-    NU_NOTA_COMP3,
-    NU_NOTA_COMP4,
-    NU_NOTA_COMP5,
-    NU_NOTA_REDACAO
-FROM {{ source('raw_enem', 'stg_enem_2024') }}
+/* Padrão de CTEs (Common Table Expressions):
+    1. import_ctes: Traz as tabelas fontes.
+    2. renamed: Renomeia, tipa e limpa os dados.
+    3. final: Seleciona o resultado.
+*/
+
+WITH source AS (
+    SELECT * FROM {{ source('raw_enem', 'stg_enem_2024') }}
+),
+
+renamed AS (
+    SELECT
+        -- Identificadores
+        CAST(NU_SEQUENCIAL AS STRING) AS student_id,
+        CAST(NU_ANO AS INT64) AS exam_year,
+        
+        -- Escola e Localização (School & Location)
+        CAST(CO_ESCOLA AS STRING) AS school_id,
+        CAST(CO_MUNICIPIO_ESC AS STRING) AS school_city_id,
+        INITCAP(NO_MUNICIPIO_ESC) AS school_city_name,
+        SG_UF_ESC AS school_state,
+        TP_DEPENDENCIA_ADM_ESC AS school_admin_type_id, -- Sugere-se criar tabela dimensão para decodificar
+        TP_LOCALIZACAO_ESC AS school_location_type_id,
+        TP_SIT_FUNC_ESC AS school_status_id,
+
+        -- Local de Prova (Exam Location)
+        CAST(CO_MUNICIPIO_PROVA AS STRING) AS exam_city_id,
+        INITCAP(NO_MUNICIPIO_PROVA) AS exam_city_name,
+        SG_UF_PROVA AS exam_state,
+
+        -- Presença (Presence) - Bool ou Int
+        TP_PRESENCA_CN AS is_present_natural_sciences,
+        TP_PRESENCA_CH AS is_present_humanities,
+        TP_PRESENCA_LC AS is_present_languages,
+        TP_PRESENCA_MT AS is_present_math,
+
+        -- Códigos da Prova (Exam Version IDs)
+        CO_PROVA_CN AS natural_sciences_exam_id,
+        CO_PROVA_CH AS humanities_exam_id,
+        CO_PROVA_LC AS languages_exam_id,
+        CO_PROVA_MT AS math_exam_id,
+
+        -- Notas (Scores) - Tratamento de Tipagem para Float
+        -- Utiliza SAFE_CAST para evitar erros se vier string vazia ou suja
+        SAFE_CAST(NU_NOTA_CN AS FLOAT64) AS natural_sciences_score,
+        SAFE_CAST(NU_NOTA_CH AS FLOAT64) AS humanities_score,
+        SAFE_CAST(NU_NOTA_LC AS FLOAT64) AS languages_score,
+        SAFE_CAST(NU_NOTA_MT AS FLOAT64) AS math_score,
+        
+        -- Notas da Redação (Essay Scores)
+        TP_STATUS_REDACAO AS essay_status_id,
+        SAFE_CAST(NU_NOTA_COMP1 AS INT64) AS essay_competence_1_score,
+        SAFE_CAST(NU_NOTA_COMP2 AS INT64) AS essay_competence_2_score,
+        SAFE_CAST(NU_NOTA_COMP3 AS INT64) AS essay_competence_3_score,
+        SAFE_CAST(NU_NOTA_COMP4 AS INT64) AS essay_competence_4_score,
+        SAFE_CAST(NU_NOTA_COMP5 AS INT64) AS essay_competence_5_score,
+        SAFE_CAST(NU_NOTA_REDACAO AS INT64) AS essay_total_score,
+
+        -- Metadados de Respostas (Opcional manter na staging se for usar para análise de itens)
+        TP_LINGUA AS foreign_language_type, -- 0=Inglês, 1=Espanhol
+        TX_RESPOSTAS_CN AS natural_sciences_answers,
+        TX_RESPOSTAS_CH AS humanities_answers,
+        TX_RESPOSTAS_LC AS languages_answers,
+        TX_RESPOSTAS_MT AS math_answers,
+        TX_GABARITO_CN AS natural_sciences_key,
+        TX_GABARITO_CH AS humanities_key,
+        TX_GABARITO_LC AS languages_key,
+        TX_GABARITO_MT AS math_key
+
+    FROM source
+)
+
+SELECT * FROM renamed
