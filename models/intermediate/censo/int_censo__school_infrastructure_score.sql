1b20ad1a9ea702bdf0531304c0b9a3d396e40fe2
Victor Capel <victorsturnichcapel@gmail.com>
2026-02-06 22:26:16 +0000

    Updates columns int_censo

--- models/intermediate/censo/int_censo__school_infrastructure_score.sql
+++ models/intermediate/censo/int_censo__school_infrastructure_score.sql
@@ -11,6 +11,7 @@ calculated AS (
     SELECT
         school_id,
         school_name,
+        dependency_type_id,
         -- Criando um score de tecnologia (0 a 3)
         (CAST(has_internet AS INT) + CAST(has_broadband AS INT) + CAST(has_it_lab AS INT)) AS tech_score,
         -- Criando um score de instalações (0 a 3)
